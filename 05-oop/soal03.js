8c1006e557727ce3b0f1b40fb833325623ce1f43
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2021-10-06 22:36:43 +0700

    nyicil tugas 5 - 7

--- 05-oop/soal03.js
+++ 05-oop/soal03.js
@@ -4,14 +4,17 @@ class Phone {
         this.storage = storage;
         this.ram = ram;
     }
+    getBrandName() {
+        return this.brand
+    }
+    setBrandName(b) {
+        this.brand = b
+    }
+    printSpecification() {
+        console.log(`Ponsel ini memiliki storage: ${this.storage} dan ram: ${this.ram}`)
+    }
+    setSpecification(s, r) {
+        this.storage = s
+        this.ram = r
+    }
 }
-
-const phone = new Phone("Skilvul Mobile co", 64, 4);
-
-console.log(phone.getBrandName());
-phone.setBrandName("SM.co")
-console.log(phone.getBrandName());
-
-phone.printSpecification();
-phone.setSpecification(32, 2);
-phone.printSpecification();
--- 05-oop/soal04.js
+++ 05-oop/soal04.js
@@ -1,31 +1,73 @@
 class Student {
-    constructor(name, email) {
+    constructor(name, gender) {
         this.name = name;
-        this.email = email;
+        this.gender = gender;
         this.courseOfferings = [];
     }
 
-    takeNewCourse() {
-
+    getIndexFromCourse(course) {
+        const indexOfCourse = this.courseOfferings.findIndex((courseOffering) => {
+            return courseOffering.course.getSubject() === course.getSubject();
+        });
+        return indexOfCourse;
     }
 
-    takeATest() {
+    takeNewCourse(course) {
+        // check if course already in array
+        const isCourseExist = this.courseOfferings.find((courseOffering) => {
+            return courseOffering.course.getSubject() === course.getSubject();
+        });
 
+        // push the course into array if the course not exist yet
+        if (this.courseOfferings.length === 0 || !isCourseExist) {
+            this.courseOfferings.push(new CourseOffering(course));
+            course.decreaseQuota();
+        }
     }
 
-    courseAttendance() {
+    courseAttendance(course) {
+        const indexOfCourse = this.getIndexFromCourse(course);
+        if (indexOfCourse >= 0) {
+            this.courseOfferings[indexOfCourse].attendance++;
+        }
+    }
 
+    takeATest(course) {
+        const indexOfCourse = this.getIndexFromCourse(course);
+        if (indexOfCourse >= 0) {
+            if (this.courseOfferings[indexOfCourse].attendance >= course.getAttendance()) {
+                this.courseOfferings[indexOfCourse].grade = Math.floor(Math.random() * 100);
+            } else {
+                console.log("please fill your absent");
+            }
+        }
     }
 }
 
 class CourseOffering {
-
+    constructor(course) {
+        this.course = course;
+        this.attendance = 0;
+        this.grade = 0;
+    }
 }
 
 class Course {
-
+    constructor(subject, quota, attendance) {
+        this.subject = subject;
+        this.quota = quota;
+        this.attendance = attendance;
+    }
+    getSubject() {
+        return this.subject;
+    }
+    getAttendance() {
+        return this.attendance;
+    }
+    decreaseQuota() {
+        this.quota--;
+    }
 }
-
 
 const biology = new Course("biology", 10, 3);
 const physics = new Course("physics", 10, 2);

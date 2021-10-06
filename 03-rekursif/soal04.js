d14b9942ecb5448b46d2585c366563e507e8e9d1
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2021-10-06 22:38:14 +0700

    nyicil tugas

--- 03-rekursif/soal04.js
+++ 03-rekursif/soal04.js
@@ -1,13 +1,17 @@
-let i = 1
-let j = 1
-
-function trianglePattern(num1, num2) {
-    var s = 'a';
-    while (s != 'z') {
-        alert("ini huruf " + s);
-        s = String.fromCharCode(s.charCodeAt(0) + 1);
-
+function trianglePattern(a, b) {
+    if (a === 0) {
+        return;
     }
+    let space = "";
+    for (let index = 0; index < b; index++) {
+        if (index < a - 1) {
+            space += " "
+        } else {
+            space += String.fromCharCode(64 + (b - a + (index - a) + 2))
+        }
+    }
+    console.log(space);
+    trianglePattern(a - 1, b)
 }
 
 trianglePattern(5, 5);

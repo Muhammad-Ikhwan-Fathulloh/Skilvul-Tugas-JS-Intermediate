9dd654553088e153a7cff50fa0fc41beb4e62400
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2021-10-08 22:10:01 +0700

    Menyicil tugas 8

--- 08-asynchronus/soal03.js
+++ 08-asynchronus/soal03.js
@@ -1,5 +1,13 @@
-const getGitHubUser = () => {
-
+const getGitHubUser = (user_id) => {
+    try {
+        let response = await fetch(`https://api.github.com/users/${user_id}`, {
+            method: 'GET',
+        });
+        let jsonResponse = await response.json();
+        return jsonResponse;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const printGitHubUser = async() => {
--- 08-asynchronus/soal04.js
+++ 08-asynchronus/soal04.js
@@ -4,8 +4,16 @@ const elCardImg = document.querySelector(".card-img-top");
 const elCardBtn = document.querySelector("#card-btn");
 const elCardTitle = document.querySelector("#card-title");
 
-const getGitHubUser = () => {
-
+const getGitHubUser = (user_id) => {
+    try {
+        let response = await fetch(`https://api.github.com/users/${user_id}`, {
+            method: 'GET',
+        });
+        let jsonResponse = await response.json();
+        return jsonResponse;
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 elFormUsername.onsubmit = (e) => {
